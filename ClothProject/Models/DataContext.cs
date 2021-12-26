7062ca90d93702f16d8d4c61a4efe3548e47fc67
Mykola Berkovskyi <nomvodafon@gmail.com>
2021-12-26 22:54:18 +0200

    Добавьте файлы проекта.

---
+++ ClothProject/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClothProject.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<ClothCategory> ClothCategories { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<ShopItem> ShopItems { get; set; }
+        public DbSet<ClothType> ClothTypes { get; set; }
+        public DbSet<Interest> Interests { get; set; }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
